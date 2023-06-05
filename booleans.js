62295c3ef909d90beb79f50d5bf4d06cb07486e7
BOSTONE069 <bostoneochieng69@gmail.com>
2023-06-05 16:17:33 -0400

    This is arrays

---
+++ booleans.js
@@ -0,0 +1,17 @@
+let isLightOn = true
+let isRaining = false
+let isHungry = false
+let isMarried = true
+let truValue = 4 > 3    // true
+let falseValue = 4 < 3  // false
+let number = 5
+number > 0
+  ? console.log(`${number} is a positive number`)
+  : console.log(`${number} is a negative number`)
+number = -5
+
+number > 0
+  ? console.log(`${number} is a positive number`)
+  : console.log(`${number} is a negative number`)
+
+
